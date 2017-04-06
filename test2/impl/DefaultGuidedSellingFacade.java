77c38dd82ce2b2ff25254b0ff6b8840b877b8b5d
junbin.liu <junbin.liu@accenture.com>
2017-04-06 12:02:42 +0800

    c5 commit1

--- test2/impl/DefaultGuidedSellingFacade.java
+++ test2/impl/DefaultGuidedSellingFacade.java
@@ -2,6 +2,7 @@
 * [y] hybris Platform
 * modify c3 commit 2
 * modify by c4 commit 1
+* modify by c5 commit1
 * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
 * All rights reserved.
 * modify c3 commit 3
